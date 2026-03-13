ab4c1755998fad127e3fb43fa683e32aeabaf924
whenigetout <mrglad99@gmail.com>
2026-03-13 07:48:27 +0530

    # Problem: 853. Car Fleet

---
+++ 853_car_fleet/python/solution2.py
@@ -0,0 +1,21 @@
+# Problem: 853. Car Fleet
+# URL: 
+# Difficulty: Unknown
+# Tags: 
+
+from typing import List, Optional
+
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        p_v = zip(position, speed)
+        p_v = sorted(p_v)
+        fleets = 0
+        while p_v:
+            car = p_v.pop()
+            fleets += 1
+            while p_v and (target - p_v[-1][0]) / (p_v[-1][1]) <= (target - car[0]) / (car[1]):
+                p_v.pop()
+        return fleets
+    
+o = Solution()
+print(o.carFleet(target = 12, position = [10,8,0,5,3], speed = [2,4,1,1,3]))
